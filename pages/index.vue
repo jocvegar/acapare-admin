e53ed0ad6eb4b7aa9f010214e1530787aa4eeb58
Jose Vega <jocvegar@gmail.com>
2021-08-04 09:52:32 -0600

    adds test interface

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
+  <v-row v-if="!loading" justify="center" align="center">
+    <v-col cols="12" sm="10" md="8">
+      <v-card class="logo d-flex justify-center">
+        <v-img max-height="350" :src="data.imageUrls[0]"></v-img>
       </v-card>
       <br />
       <v-card>
@@ -11,7 +10,13 @@
           HOLIS
         </v-card-title>
         <v-card-text>
-          <p>name: {{ name }}</p>
+          <h2 class="text-center pb-10">
+            {{ data.name }}
+          </h2>
+
+          <h4 class="text-center pb-10">
+            Un total de {{ data.price }} 0 {{ data.price + 5 }}
+          </h4>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
             velit laboriosam minus explicabo, eaque amet consequuntur modi
@@ -43,16 +48,30 @@
 </template>
 
 <script lang="ts">
+type IProduct = {
+  name: string;
+  price: number;
+  visible: boolean;
+  imageUrls?: string[];
+};
 import Vue from "vue";
 export default Vue.extend({
   data() {
-    const name: string = "";
+    const data: IProduct = {
+      name: "",
+      price: 0,
+      visible: false,
+      imageUrls: []
+    };
+    const loading: boolean = true;
     return {
-      name
+      data,
+      loading
     };
   },
   mounted() {
     this.getMessage();
+    this.loading = false;
   },
   methods: {
     async getMessage() {
@@ -60,8 +79,7 @@ export default Vue.extend({
         .collection("products")
         .doc("test");
       const testDoc = await productTestRef.get();
-      const data = testDoc.data();
-      this.name = data ? data.name : "";
+      this.data = testDoc.data();
     }
   }
 });
